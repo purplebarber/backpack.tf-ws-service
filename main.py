6dcc9b059a1a359a55fd75f9e8af59001efa3741
Purple <87976511+purplebarber@users.noreply.github.com>
2023-07-30 00:20:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+from bptf_websocket import BptfWebSocket
 from json import load
-from database import CassandraManager
+from asyncio import run
 
 
-def main():
-    # Load config
+async def main():
     with open('config.json', 'r') as f:
         config = load(f)
 
+    WEBSOCKET_URL = config['websocketURL']
     CLIENT_ID = config['clientId']
     CLIENT_SECRET = config['secret']
     SECURE_CONNECT_BUNDLE = config['secureConnectBundlePath']
     KEYSPACE = config['keyspace']
     TABLE_NAME = config['tableName']
 
-    # Create a new instance of the CassandraManager class
-    cassandra_manager = CassandraManager(CLIENT_ID, CLIENT_SECRET, SECURE_CONNECT_BUNDLE, KEYSPACE, TABLE_NAME)
-    cassandra_manager.delete_listing("Tough Stuff Muffs", 1)
+    bptf = BptfWebSocket(CLIENT_ID, CLIENT_SECRET, SECURE_CONNECT_BUNDLE, KEYSPACE, TABLE_NAME, WEBSOCKET_URL)
+
+    try:
+        await bptf.parse_websocket_events()
+    finally:
+        await bptf.close_connection()
+
 
 if __name__ == '__main__':
-    main()
+    run(main())
