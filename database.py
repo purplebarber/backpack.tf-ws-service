c47607b26b131dd5387f4e452d448b2e3f053ee0
Purple <87976511+purplebarber@users.noreply.github.com>
2023-07-30 00:20:57 +0400

    Update database.py

--- database.py
+++ database.py
@@ -6,10 +6,13 @@ from json import loads, dumps
 
 class CassandraManager:
     def __init__(self, client_id, client_secret, secure_connect_bundle_path, keyspace, table_name):
-        cloud_config = {'secure_connect_bundle': secure_connect_bundle_path}
+        cloud_config = {
+            'secure_connect_bundle': secure_connect_bundle_path
+        }
         auth_provider = PlainTextAuthProvider(client_id, client_secret)
-        self.cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-        self.session = self.cluster.connect(keyspace)  # Connect to the specified keyspace
+        self.cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider, control_connection_timeout=30,
+                               connect_timeout=30)
+        self.session = self.cluster.connect(keyspace)
         self.keyspace = keyspace
         self.table_name = table_name
         self.create_table()
@@ -23,28 +26,34 @@ class CassandraManager:
         """
         self.session.execute(query)
 
-    def insert_listing(self, sku, listing_id, listing_data) -> None:
+    async def insert_listing(self, sku, listing_id, listing_data) -> None:
         query = f"""
             UPDATE {self.keyspace}.{self.table_name}
             SET listings[%s] = %s
             WHERE sku = %s
         """
-        # Use SimpleStatement with the map data for proper handling
-        statement = SimpleStatement(query)
-        self.session.execute(statement, (str(listing_id), dumps(listing_data), str(sku)))
 
-    def get_listing(self, sku, listing_id) -> dict | None:
+        statement = SimpleStatement(query)
+        return self.session.execute_async(statement, (str(listing_id), dumps(listing_data), str(sku)))
+
+    async def get_listing(self, sku, listing_id) -> dict | None:
         query = f"SELECT listings['{str(listing_id)}'] FROM {self.keyspace}.{self.table_name} WHERE sku = '{str(sku)}'"
         result = self.session.execute(query)
-        return loads(result.one()[0]) if result else None
+        try:
+            return loads(result.one()[0]) if result else None
+        except TypeError:
+            return None
 
-    def delete_listing(self, sku, listing_id) -> None:
+    async def delete_listing(self, sku, listing_id) -> None:
+        listing_id_set = {str(listing_id)}
+
         query = f"""
             UPDATE {self.keyspace}.{self.table_name}
-            SET listings = listings - {{ '{str(listing_id)}' }}
-            WHERE sku = '{str(sku)}'
+            SET listings = listings - %s
+            WHERE sku = %s
         """
-        self.session.execute(query)
+        statement = SimpleStatement(query)
+        return self.session.execute_async(statement, (listing_id_set, str(sku)))
 
     def close_connection(self) -> None:
         self.session.shutdown()
